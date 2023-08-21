3aab2f24c2b4662b5ecc4a258c2cb5511c0c85e2
Murillo <murillons11@gmail.com>
2023-08-21 08:52:50 -0300

    Junção_obj_func

--- assets/js/Util.js
+++ assets/js/Util.js
@@ -13,6 +13,38 @@ class Obj {
     }
 }
 
+class Bee extends Obj{
+    dir = 0
+    pts = 0
+    vidas = 3
+
+    move(){
+        this.x += this.dir
+        if(this.x <= 16){
+            this.x = 16
+        }else if (this.x >= 400){
+            this.x = 400
+        }
+        if(this.y <= 16){
+            this.y = 16
+        }else if (this.x >= 600){
+            this.x = 600
+        }
+    }
+
+    colid(objeto){
+        if((this.x < objeto.x + objeto.width)&&
+            (this.x + this.width > objeto.x)&&
+            (this.y < objeto.y + objeto.height)&&
+            (this.y + this.height > objeto.y)){
+            return true       
+        }else{
+            return false
+        }
+
+    }
+}
+
 class BG extends Obj {
     
 }

a13fd8dd2b34f7f22a46061bff376a880769c4fd
antonio murara zabala <antoniomurar4@gmail.com>
2023-08-21 08:44:58 -0300

    Criando a classe Obj

---
+++ assets/js/Util.js
@@ -0,0 +1,26 @@
+class Obj {
+    constructor(x,y,w,h,at){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.at = at
+    }
+    des_obj(){
+        let img = new Image()
+        img.src = this.at
+        des.drawImage(img,this.x,this.y,this.w,this.h)
+    }
+}
+
+class BG extends Obj {
+    
+}
+
+class Player extends Obj {
+
+}
+
+class Tiro extends Obj {
+
+}

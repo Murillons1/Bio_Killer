4586e8638017d9b7a6610a1aa2caf5e55239f18a
antonio murara zabala <antoniomurar4@gmail.com>
2023-08-23 10:54:56 -0300

    funcionalidades de movimento do player, lacaio, planta e background

--- assets/js/Util.js
+++ assets/js/Util.js
@@ -1,4 +1,7 @@
 class Obj {
+    frame = 1
+    tempo = 0
+
     constructor(x,y,w,h,at){
         this.x = x
         this.y = y
@@ -12,27 +15,45 @@ class Obj {
         img.src = this.at
         des.drawImage(img,this.x,this.y,this.w,this.h)
     }
+    anim(nome){
+        this.tempo +=1
+        if(this.tempo > 12){
+            this.tempo = 0
+            this.frame +=1
+        }
+        if(this.frame>4){
+            this.frame=1
+        }
+        this.img = "assets/img/player/Raider_1/"+nome+this.frame+".png"
+    }
 }
 
 class Robb extends Obj{
-    dir = 0
+    dir_x = 0
+    dir_y = 0
     pts = 0
     vidas = 3
 
     move(){
-        this.x += this.dir
-        if(this.x <= 16){
-            this.x = 16
-        }else if (this.x >= 400){
-            this.x = 400
+        // EIXO X
+        this.x += this.dir_x
+        console.log(this.x)
+        if(this.x <= -5){
+            this.x = -5
+        }else if(this.x >= 740){
+            this.x = 740
         }
-        if(this.y <= 16){
-            this.y = 16
-        }else if (this.x >= 600){
-            this.x = 600
+        // EIXO Y
+        this.y += this.dir_y
+        console.log(this.y)
+        if(this.y <= 0){
+            this.y = 0
+        }else if (this.y >= 390){
+            this.y = 390
         }
     }
 
+    
     colid(objeto){
         if((this.x < objeto.x + objeto.width)&&
             (this.x + this.width > objeto.x)&&
@@ -41,9 +62,9 @@ class Robb extends Obj{
             return true       
         }else{
             return false
-        }
-
+        }     
     }
+    
 }
 
 class BG extends Obj {
@@ -53,16 +74,71 @@ class BG extends Obj {
             this.x = inicio
         }
     }
+
+    mov1(inicio,fim){
+        this.x += 4
+        if(this.x > fim){
+            this.x = inicio
+        }
+    }
 }
 
 class Lacaio extends Obj {
 
+    move(){
+        this.x -= 3
+        if(this.x <= 0){
+            this.x = 770
+            this.y = Math.random() * (400 - 0) 
+        }
+    }
+    
+    recomeca(){
+        this.y = -200
+        this.x = Math.random() * (400 - 0) 
+    }
+}
+
+class Tiro extends Obj{
+    des_tiro(){
+        des.fillStyle = this.at
+        des.fillRect(this.x, this.y, this.w, this.h)
+    }
+
+    mov(){
+        this.x += 10
+    }
+    // acrescentado, verificar
+    colid(objeto) {
+        // Verifica colisão entre duas caixas delimitadoras retangulares
+        return (
+            this.x < objeto.x + objeto.w &&
+            this.x + this.w > objeto.x &&
+            this.y < objeto.y + objeto.h &&
+            this.y + this.h > objeto.y
+        )
+    }
+}
+
+class Texto{
+    des_text(texto,x,y,cor,font){
+        des.font = font
+        des.fillStyle = cor
+        des.fillText(texto,x,y)
+    }
 }
 
 class Planta extends Obj {
-
-}
-
-class Tiro extends Obj {
-
+    move(){
+        this.x -= 1
+        if(this.x <= 0){
+            this.x = 770
+            this.y = Math.random() * (400 - 0) 
+        }
+    }
+    
+    recomeca(){
+        this.y = -200
+        this.x = Math.random() * (400 - 0) 
+    }
 }
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,71 +1,108 @@
     const des = document.getElementById('des').getContext('2d')
 
-    let bg1 = new BG(0,0,800,500,'./assets/img/background/winter_3/11.png')
-    let robb = new Robb(0,0,100,100,'./assets/img/player/Raider_1/Walk.png')
-    let lacaio = new Lacaio(0,0,50,50,)
-    let planta = new Planta(0,0,50,50,)
+    // CÉU
+    const bg1 = new BG(0,0,800,500,'./assets/img/background/nature_1/1.png')
+    const bg2 = new BG(0,0,800,500,'./assets/img/background/nature_1/2.png')
+    const bg3 = new BG(0,0,800,500,'./assets/img/background/nature_1/3.png')
+    const bg4 = new BG(0,0,800,500,'./assets/img/background/nature_1/4.png')
+    const bg5 = new BG(0,0,800,500,'./assets/img/background/sky_3/7.png')
+    const bg6 = new BG(0,0,800,500,'./assets/img/background/sky_3/4.png')
+    let robb = new Robb(0,390,64,110,'./assets/img/player/Raider_1/robb1.png')
+    let lacaio = new Lacaio(0,0,80,100,'./assets/img/player/lacaio/lacaio.png')
+    let planta = new Planta(0,0,80,80,'./assets/img/player/lacaio/cogumelo.png')
+    let txt_pts = new Texto()
+    let pts = new Texto()
+    let txt_vidas = new Texto()
+    let n_vidas = new Texto()
+    let txt_municao = new Texto()
+    let municao = new Texto()
     let jogar = true
 
-    document.addEventListener('keydown', (event)=>{
-        if(event.key === 'a'){
-            robb.dir = - 5
-        }else if(event.key === 'd'){
-            robb.dir = 5
-        }else if(event.key === 's'){
-        }else if(event.key === 'w'){
-        }
+    const grupoTiros = []
 
-    })
-    document.addEventListener('keydown', (event)=>{
-        if(event.key === 'ArrowLeft'){
-            // console.log('soltou a tecla "a" ')
-            robb.dir = 0
-        }else if(event.key === 'ArrowRigth'){
-            // console.log('soltou a tecla "d" ')
-            robb.dir = 0
-        }else if(event.key === 'ArrowDown'){
-            console.log('soltou a tecla "ArrowDown" ')
-        }else if(event.key === 'ArrowUp'){
-            console.log('soltou a tecla "ArrowUp" ')
+    let isJumping = false;
+
+    // MOVIMENTO
+
+    document.addEventListener('keydown', (ev)=>{
+        if(ev.key === 'a'){
+            robb.dir_x -= 5;
         }
+        if(ev.key === 'd'){
+            robb.dir_x += 5;
+        }
+        if(ev.key === 's'){
+            robb.dir_y += 5;
+        }
+        if(ev.key === 'w' && !isJumping){  // Verifique se não está pulando para evitar saltos consecutivos
+            isJumping = true;  // Define o estado de pulo para verdadeiro
+            robb.dir_y -= 10;  // Altere o valor para ajustar a altura do pulo
+        }
+    });
+    
+    document.addEventListener('keyup', (ev)=>{
+        if(ev.key === 'a'){
+            robb.dir_x = 0;
+        }
+        if(ev.key === 'd'){
+            robb.dir_x = 0;
+        }
+        if(ev.key === 's'){
+            robb.dir_x = 0;
+        }
+        if(ev.key === 'w'){
+            robb.dir_y = 0;
+            isJumping = false;  // Reinicie o estado de pulo quando a tecla é liberada
+        }
+    });
+    document.addEventListener('keypress', (ev)=>{
+        if (ev.key === 'l') {
+            let tiro = new Tiro(robb.x - 4 + robb.w / 2, robb.y, 8, 16, 'yellow')
+            grupoTiros.push(tiro)
+        }
+        som1.play()
     })
 
-    document.addEventListener('keyup', (event)=>{
-        if(event.key === 'a'){
-            // console.log('soltou a tecla "a" ')
-            robb.dir = 0
-        }else if(event.key === 'd'){
-            // console.log('soltou a tecla "d" ')
-            robb.dir = 0
-        }else if(event.key === 's'){
-            console.log('soltou a tecla "s" ')
-        }else if(event.key === 'w'){
-            console.log('soltou a tecla "w" ')
+    function pontos(){
+        if(robb.point(disco1)){
+            robb.pts +=1
+        }else if(robb.point(disco2)){
+            robb.pts +=1
+        }else if(robb.point(disco3)){
+            robb.pts +=1
         }
-    })
-    document.addEventListener('keyup', (event)=>{
-        if(event.key === 'ArrowLeft'){
-            // console.log('soltou a tecla "a" ')
-            robb.dir = 0
-        }else if(event.key === 'ArrowRigth'){
-            // console.log('soltou a tecla "d" ')
-            robb.dir = 0
-        }else if(event.key === 'ArrowDown'){
-            console.log('soltou a tecla "ArrowDown" ')
-        }else if(event.key === 'ArrowUp'){
-            console.log('soltou a tecla "ArrowUp" ')
+    }
+
+    function tiros_robb() {
+        for (let i = 0; i < grupoTiros.length; i++) {
+            for (let j = 0; j < discos.length; j++) {
+                if (grupoTiros[i].colid(discos[j])) {
+                    // Se houver colisão, remove o tiro e o disco do jogo
+                    grupoTiros.splice(i, 1)
+                    discos.splice(j, 1)
+    
+                    // Incrementa os pontos do jogador
+                    robb.pts += 1
+                }
+            }
         }
-    })
+    }
+
     function desenha(){
         bg1.des_obj()
+        bg2.des_obj()
+        bg3.des_obj()
+        bg4.des_obj()
+        bg5.des_obj()
+        bg6.des_obj()
         robb.des_obj()
-        // if(jogar){        
-        //     robb.des_obj()
-        //     lacaio.des_obj()
-        //     planta.des_obj()
-        // }
+        lacaio.des_obj()
+        planta.des_obj()
+        grupoTiros.forEach((tiro)=>{
+            tiro.des_tiro()
+        })
     }
-    // colisao
+
     function colisao(){
         if(robb.colid(lacaio)){
             lacaio.recomeca()
@@ -86,21 +123,24 @@
 
 
     function atualiza(){
-        // if(jogar){
-        //     robb.move()
-        //     robb.anim('robb')
-        //     lacaio.move()
-        //     lacaio.anim('lacaio')
-        //     planta.move()
-        //     planta.anim('planta')
-        //     colisao()
-        // }
-        
+        robb.move()
+        lacaio.move()
+        planta.move()
+        robb.anim('robb')
+        bg1.mov(-500,800)
+        bg5.mov1(-1000,800)
+        grupoTiros.forEach((tiro)=>{
+            tiro.mov()
+            if(tiro.y <= -50){
+                grupoTiros.splice(tiro[0],1)
+            }
+        })
     }
 
     function main(){
-        des.clearRect(0,0,800,500)
+        des.clearRect(0,0,des.width,des.height)
         desenha()
+        atualiza()
         requestAnimationFrame(main)
     }
     main()
